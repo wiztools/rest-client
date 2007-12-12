b964e9ee52cb2cdac2c396e5b94c3040432766b0
ravis.cec <ravis.cec@08545216-953f-0410-99bf-93b34cd2520f>
2007-12-12 07:01:30 +0000

    modified XMLUtil.java
    git-svn-id: https://rest-client.googlecode.com/svn/trunk@42 08545216-953f-0410-99bf-93b34cd2520f

--- src/main/java/org/wiztools/restclient/XMLUtil.java
+++ src/main/java/org/wiztools/restclient/XMLUtil.java
@@ -2,12 +2,13 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package org.wiztools.restclient;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
 import java.util.Map;
 import javax.xml.parsers.DocumentBuilder;
@@ -23,14 +24,18 @@ import javax.xml.transform.stream.StreamResult;
 import org.w3c.dom.DOMImplementation;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 /**
  *
  * @author rsubramanian
  */
 public final class XMLUtil {
-    public static Document request2XML(final RequestBean bean) 
+
+    public static Document request2XML(final RequestBean bean)
             throws ParserConfigurationException {
         Document xmldoc = null;
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
@@ -41,11 +46,11 @@ public final class XMLUtil {
         Node n = null;
         Element subChild = null;
         Node node = null;
-        
+
         xmldoc = impl.createDocument(null, "rest-client", null);
         Element root = xmldoc.getDocumentElement();
-        
-        request = xmldoc.createElementNS(null , "request");
+
+        request = xmldoc.createElementNS(null, "request");
         // creating the URL child element
         e = xmldoc.createElementNS(null, "URL");
         n = xmldoc.createTextNode(bean.getUrl().toString());
@@ -71,7 +76,7 @@ public final class XMLUtil {
             e.appendChild(n);
             request.appendChild(e);
         }
-        
+
         // creating the auth-host child element
         String authHost = bean.getAuthHost();
         if (authHost != null) {
@@ -136,16 +141,96 @@ public final class XMLUtil {
             request.appendChild(e);
         }
         root.appendChild(request);
-        
+
         return xmldoc;
     }
-    
-    public static RequestBean xml2Request(final Document doc){
-        return null;
+
+    public static RequestBean xml2Request(final Document doc)
+            throws MalformedURLException {
+        RequestBean requestBean = new RequestBean();
+        NodeList elements = null;
+        Node node = null;
+
+        //get url
+        elements = doc.getElementsByTagName("URL");
+        for (int i = 0; i < elements.getLength(); i++) {
+            node = elements.item(i);
+            URL url = new URL(node.getTextContent());
+            requestBean.setUrl(url);
+        }
+
+        //get method
+        elements = doc.getElementsByTagName("method");
+        for (int i = 0; i < elements.getLength(); i++) {
+            node = elements.item(i);
+            requestBean.setMethod(node.getTextContent());
+        }
+
+        //get auth-methods
+        elements = doc.getElementsByTagName("auth-methods");
+        for (int i = 0; i < elements.getLength(); i++) {
+            node = elements.item(i);
+            String[] authenticationMethods = node.getTextContent().split(",");
+            for (int j = 0; j < authenticationMethods.length; j++) {
+                requestBean.addAuthMethod(authenticationMethods[j]);
+            }
+        }
+
+        //get auth-host
+        elements = doc.getElementsByTagName("auth-host");
+        for (int i = 0; i < elements.getLength(); i++) {
+            node = elements.item(i);
+            requestBean.setAuthHost(node.getTextContent());
+        }
+
+        //get auth-realm
+        elements = doc.getElementsByTagName("auth-realm");
+        for (int i = 0; i < elements.getLength(); i++) {
+            node = elements.item(i);
+            requestBean.setAuthRealm(node.getTextContent());
+        }
+
+        //get auth-username
+        elements = doc.getElementsByTagName("auth-username");
+        for (int i = 0; i < elements.getLength(); i++) {
+            node = elements.item(i);
+            requestBean.setAuthUsername(node.getTextContent());
+        }
+
+        //get password
+        elements = doc.getElementsByTagName("auth-password");
+        for (int i = 0; i < elements.getLength(); i++) {
+            node = elements.item(i);
+            requestBean.setAuthPassword(node.getTextContent().toCharArray());
+        }
+
+        //get headers
+        elements = doc.getElementsByTagName("header");
+        for (int i = 0; i < elements.getLength(); i++) {
+            node = elements.item(i);
+            NamedNodeMap nodeMap = node.getAttributes();
+            Node key = nodeMap.getNamedItem("key");
+            Node value = nodeMap.getNamedItem("value");
+            requestBean.addHeader(key.getNodeValue(), value.getNodeValue());
+        }
+
+        //get body
+        elements = doc.getElementsByTagName("body");
+        for (int i = 0; i < elements.getLength(); i++) {
+            node = elements.item(i);
+            NamedNodeMap nodeMap = node.getAttributes();
+            Node contentType = nodeMap.getNamedItem("content-type");
+            Node charSet = nodeMap.getNamedItem("char-set");
+            requestBean.setBody(new ReqEntityBean(contentType.getNodeValue(),
+                    charSet.getNodeValue(), node.getTextContent()));
+        }
+
+        return requestBean;
     }
-    
-    public static Document response2XML(final ResponseBean bean) throws ParserConfigurationException{
-        
+
+    public static Document response2XML(final ResponseBean bean)
+            throws ParserConfigurationException {
+
         Document xmldoc = null;
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         DocumentBuilder builder = factory.newDocumentBuilder();
@@ -154,19 +239,19 @@ public final class XMLUtil {
         Node n = null;
         Element response = null;
         Element subChild = null;
-        
+
         xmldoc = impl.createDocument(null, "rest-client", null);
         Element root = xmldoc.getDocumentElement();
-        
+
         response = xmldoc.createElementNS(null, "response");
-        
+
         // creating the status child element
         e = xmldoc.createElementNS(null, "status");
         e.setAttributeNS(null, "code", String.valueOf(bean.getStatusCode()));
         n = xmldoc.createTextNode(bean.getStatusLine());
         e.appendChild(n);
         response.appendChild(e);
-        
+
         // creating the headers child element
         Map<String, String> headers = bean.getHeaders();
         if (!headers.isEmpty()) {
@@ -180,27 +265,59 @@ public final class XMLUtil {
             }
             response.appendChild(e);
         }
-        
+
         //creating the body child element
         String responseBody = bean.getResponseBody();
-        if(responseBody != null){
+        if (responseBody != null) {
             e = xmldoc.createElementNS(null, "body");
             n = xmldoc.createTextNode(responseBody);
             e.appendChild(n);
             response.appendChild(e);
         }
-        
+
         root.appendChild(response);
-        
+
         return xmldoc;
     }
-    
-    public static ResponseBean xml2Response(final Document doc){
-        return null;
+
+    public static ResponseBean xml2Response(final Document doc) {
+        ResponseBean responseBean = new ResponseBean();
+        NodeList elements = null;
+        Node node = null;
+
+        //get status line and status code
+        elements = doc.getElementsByTagName("status");
+        for (int i = 0; i < elements.getLength(); i++) {
+            node = elements.item(i);
+            responseBean.setStatusLine(node.getTextContent());
+            NamedNodeMap nodeMap = node.getAttributes();
+            Node n = nodeMap.getNamedItem("code");
+            responseBean.setStatusCode(Integer.parseInt(n.getNodeValue()));
+        }
+
+        //get headers
+        elements = doc.getElementsByTagName("header");
+        for (int i = 0; i < elements.getLength(); i++) {
+            node = elements.item(i);
+            NamedNodeMap nodeMap = node.getAttributes();
+            Node key = nodeMap.getNamedItem("key");
+            Node value = nodeMap.getNamedItem("value");
+            responseBean.addHeader(key.getNodeValue(), value.getNodeValue());
+        }
+
+        //get body
+        elements = doc.getElementsByTagName("body");
+        for (int i = 0; i < elements.getLength(); i++) {
+            node = elements.item(i);
+            System.out.println("body content" + node.getTextContent());
+            responseBean.setResponseBody(node.getTextContent());
+        }
+
+        return responseBean;
     }
-    
-    public static void writeXML(final Document doc, final File f) 
-            throws IOException, TransformerConfigurationException, TransformerException{
+
+    public static void writeXML(final Document doc, final File f)
+            throws IOException, TransformerConfigurationException, TransformerException {
         DOMSource domSource = new DOMSource(doc);
         FileOutputStream out = new FileOutputStream(f);
         StreamResult streamResult = new StreamResult(out);
@@ -210,22 +327,38 @@ public final class XMLUtil {
         serializer.setOutputProperty(OutputKeys.INDENT, "yes");
         serializer.transform(domSource, streamResult);
     }
-    
-    public static Document getDocumentFromFile(final File f) throws IOException{
-        return null;
+
+    public static Document getDocumentFromFile(final File f) throws IOException,
+            ParserConfigurationException, SAXException {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document doc = builder.parse(f);
+        return doc;
     }
-    
+
     public static void writeRequestXML(final RequestBean bean, final File f)
-            throws IOException, ParserConfigurationException, TransformerConfigurationException,
-            TransformerException{
+            throws IOException, ParserConfigurationException, 
+            TransformerConfigurationException, TransformerException {
         Document doc = request2XML(bean);
         writeXML(doc, f);
     }
-    
+
     public static void writeResponseXML(final ResponseBean bean, final File f)
-            throws IOException, ParserConfigurationException, TransformerConfigurationException,
-            TransformerException{
+            throws IOException, ParserConfigurationException, 
+            TransformerConfigurationException, TransformerException {
         Document doc = response2XML(bean);
         writeXML(doc, f);
+    }
+    
+    public static void writeXMLRequest(final File f, RequestBean bean) 
+            throws IOException, ParserConfigurationException, SAXException{
+        Document doc = getDocumentFromFile(f);
+        bean = xml2Request(doc);
+    }
+    
+    public static void writeXMLResponse(final File f, ResponseBean bean) 
+            throws IOException, ParserConfigurationException, SAXException{
+        Document doc = getDocumentFromFile(f);
+        bean = xml2Response(doc);
     }
 }
