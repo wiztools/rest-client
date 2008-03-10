60e3233f047e3322dd2779ba4e7d61032299a5da
subwiz <subwiz@08545216-953f-0410-99bf-93b34cd2520f>
2008-03-10 15:54:22 +0000

    SplashScreen-Java mechanism works.
    git-svn-id: https://rest-client.googlecode.com/svn/trunk@161 08545216-953f-0410-99bf-93b34cd2520f

--- src/main/java/org/wiztools/restclient/Main.java
+++ src/main/java/org/wiztools/restclient/Main.java
@@ -1,9 +1,7 @@
 package org.wiztools.restclient;
 
-import javax.swing.ImageIcon;
 import org.wiztools.restclient.ui.RESTFrame;
 import javax.swing.SwingUtilities;
-import org.wiztools.restclient.ui.SplashScreen;
 
 /**
  *
@@ -15,14 +13,6 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // SplashScreen display
-        ImageIcon ii = new ImageIcon(Main.class.getClassLoader().getResource("org/wiztools/restclient/Splash.png"));
-        final SplashScreen ss = new SplashScreen(ii);
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                ss.setVisible(true);
-            }
-        });
         
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
@@ -30,13 +20,6 @@ public class Main {
             }
         });
         
-        // SplashScreen dispose
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                ss.setVisible(false);
-                ss.dispose();
-            }
-        });
     }
 
 }
