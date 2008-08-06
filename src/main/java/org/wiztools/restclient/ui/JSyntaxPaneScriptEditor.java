bb000338aca1e65ae9d64d921ed51860574fd2eb
subwiz <subwiz@08545216-953f-0410-99bf-93b34cd2520f>
2008-08-06 04:53:31 +0000

    Adding JSyntaxPane dependency.
    git-svn-id: https://rest-client.googlecode.com/svn/trunk@258 08545216-953f-0410-99bf-93b34cd2520f

---
+++ src/main/java/org/wiztools/restclient/ui/JSyntaxPaneScriptEditor.java
@@ -0,0 +1,44 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.wiztools.restclient.ui;
+
+import javax.swing.JComponent;
+import javax.swing.JEditorPane;
+import jsyntaxpane.SyntaxKit;
+
+/**
+ *
+ * @author subwiz
+ */
+class JSyntaxPaneScriptEditor implements ScriptEditor {
+    
+    private JEditorPane jep = new JEditorPane();
+    
+    JSyntaxPaneScriptEditor(String type){
+        jep.setEditorKit(new SyntaxKit(type));
+    }
+
+    public JComponent getEditorView() {
+        return jep;
+    }
+
+    public String getText() {
+        return jep.getText();
+    }
+
+    public void setText(String text) {
+        jep.setText(text);
+    }
+
+    public void setCaretPosition(int offset) {
+        jep.setCaretPosition(offset);
+    }
+
+    public void setEditable(boolean editable) {
+        jep.setEditable(editable);
+    }
+
+}
