330f736fc8224e9cc9b516fd5b94936328228b77
subwiz <subwiz@08545216-953f-0410-99bf-93b34cd2520f>
2008-10-09 15:23:04 +0000

    Version number changed to 2.3
    git-svn-id: https://rest-client.googlecode.com/svn/trunk@315 08545216-953f-0410-99bf-93b34cd2520f

--- src/main/java/org/wiztools/restclient/xml/XMLUtil.java
+++ src/main/java/org/wiztools/restclient/xml/XMLUtil.java
@@ -45,7 +45,7 @@ public final class XMLUtil {
     private static final Logger LOG = Logger.getLogger(XMLUtil.class.getName());
     
     private static final String[] VERSIONS = new String[]{
-        "2.0", "2.1", "2.2a1", "2.2a2", RCConstants.VERSION};
+        "2.0", "2.1", "2.2a1", "2.2a2", "2.2", RCConstants.VERSION};
     
     public static final String XML_MIME = "application/xml";
     public static final String XML_DEFAULT_ENCODING = "UTF-8";
