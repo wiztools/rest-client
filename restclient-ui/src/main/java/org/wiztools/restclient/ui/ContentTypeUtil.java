c5bc0dbc4ddf1bfbfafd598595a0ceefb3eca177
subwiz <subwiz@08545216-953f-0410-99bf-93b34cd2520f>
2011-08-15 13:34:36 +0000

    [issue: 111] JSON and XML content-type detection now also has support for content types having ; in their value.
    git-svn-id: https://rest-client.googlecode.com/svn/trunk@510 08545216-953f-0410-99bf-93b34cd2520f

--- restclient-ui/src/main/java/org/wiztools/restclient/ui/ContentTypeUtil.java
+++ restclient-ui/src/main/java/org/wiztools/restclient/ui/ContentTypeUtil.java
@@ -5,18 +5,27 @@ package org.wiztools.restclient.ui;
  * @author subwiz
  */
 final class ContentTypeUtil {
-    static boolean isXmlContentType(String contentType) {
-        if(contentType.startsWith("application/xml")
-                || contentType.startsWith("text/xml")
-                || contentType.endsWith("+xml")){
+    
+    private static String getContentTypeBeforeSemiColon(String contentType) {
+        return (contentType.indexOf(';') != -1)?
+                contentType.split(";")[0]:
+                contentType;
+    }
+    
+    static boolean isXmlContentType(final String contentType) {
+        final String ct = getContentTypeBeforeSemiColon(contentType);
+        if(ct.startsWith("application/xml")
+                || ct.startsWith("text/xml")
+                || ct.endsWith("+xml")){
             return true;
         }
         return false;
     }
     
-    static boolean isJsonContentType(String contentType) {
-        if(contentType.startsWith("application/json")
-                || contentType.endsWith("+json")){
+    static boolean isJsonContentType(final String contentType) {
+        final String ct = getContentTypeBeforeSemiColon(contentType);
+        if(ct.startsWith("application/json")
+                || ct.endsWith("+json")){
             return true;
         }
         return false;
