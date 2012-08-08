b09b36a89311b81f014e7eba668248b04ea7c63d
subwiz <subwiz@08545216-953f-0410-99bf-93b34cd2520f>
2012-08-08 16:19:21 +0000

    two body panel implementations.
    git-svn-id: https://rest-client.googlecode.com/svn/trunk@662 08545216-953f-0410-99bf-93b34cd2520f

---
+++ restclient-ui/src/main/java/org/wiztools/restclient/ui/ReqBodyPanelFile.java
@@ -0,0 +1,25 @@
+package org.wiztools.restclient.ui;
+
+import javax.swing.JPanel;
+import org.wiztools.restclient.ReqEntity;
+
+/**
+ *
+ * @author subwiz
+ */
+public class ReqBodyPanelFile extends JPanel implements ReqBodyOps {
+    @Override
+    public void enable() {
+        // enable control
+    }
+    
+    @Override
+    public void disable() {
+        // disable control
+    }
+    
+    @Override
+    public ReqEntity getEntity() {
+        return null;
+    }
+}
---
+++ restclient-ui/src/main/java/org/wiztools/restclient/ui/ReqBodyPanelMultipart.java
@@ -0,0 +1,25 @@
+package org.wiztools.restclient.ui;
+
+import javax.swing.JPanel;
+import org.wiztools.restclient.ReqEntity;
+
+/**
+ *
+ * @author subwiz
+ */
+public class ReqBodyPanelMultipart extends JPanel implements ReqBodyOps {
+    @Override
+    public void enable() {
+        // enable control
+    }
+    
+    @Override
+    public void disable() {
+        // disable control
+    }
+    
+    @Override
+    public ReqEntity getEntity() {
+        return null;
+    }
+}
