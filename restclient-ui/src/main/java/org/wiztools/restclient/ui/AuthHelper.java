7947ae206e2bcf5994f14d6ae5b4d92b1c8fe199
subwiz <subwiz@08545216-953f-0410-99bf-93b34cd2520f>
2012-08-09 15:30:27 +0000

    made public .
    git-svn-id: https://rest-client.googlecode.com/svn/trunk@684 08545216-953f-0410-99bf-93b34cd2520f

--- restclient-ui/src/main/java/org/wiztools/restclient/ui/AuthHelper.java
+++ restclient-ui/src/main/java/org/wiztools/restclient/ui/AuthHelper.java
@@ -7,7 +7,7 @@ import org.wiztools.restclient.HTTPAuthMethod;
  *
  * @author subwiz
  */
-class AuthHelper {
+public class AuthHelper {
     static final String NONE = "None";
     static final String BASIC = "BASIC";
     static final String DIGEST = "DIGEST";
@@ -16,45 +16,45 @@ class AuthHelper {
     
     private static final String[] ALL = new String[]{NONE, BASIC, DIGEST, NTLM, OAUTH2_BEARER};
     
-    static String[] getAll() {
+    public static String[] getAll() {
         return ALL;
     }
     
-    static boolean isBasicOrDigest(List<HTTPAuthMethod> authMethods) {
+    public static boolean isBasicOrDigest(List<HTTPAuthMethod> authMethods) {
         return authMethods.contains(HTTPAuthMethod.BASIC)
                 || authMethods.contains(HTTPAuthMethod.DIGEST);
     }
     
-    static boolean isNtlm(List<HTTPAuthMethod> authMethods) {
+    public static boolean isNtlm(List<HTTPAuthMethod> authMethods) {
         return authMethods.contains(HTTPAuthMethod.NTLM);
     }
     
-    static boolean isBearer(List<HTTPAuthMethod> authMethods) {
+    public static boolean isBearer(List<HTTPAuthMethod> authMethods) {
         return authMethods.contains(HTTPAuthMethod.OAUTH_20_BEARER);
     }
     
     // String methods:
-    static boolean isNone(String input) {
+    public static boolean isNone(String input) {
         return NONE.equals(input);
     }
     
-    static boolean isBasicOrDigest(String input) {
+    public static boolean isBasicOrDigest(String input) {
         return isBasic(input) || isDigest(input);
     }
     
-    static boolean isBasic(String input) {
+    public static boolean isBasic(String input) {
         return BASIC.equals(input);
     }
     
-    static boolean isDigest(String input) {
+    public static boolean isDigest(String input) {
         return DIGEST.equals(input);
     }
     
-    static boolean isNtlm(String input) {
+    public static boolean isNtlm(String input) {
         return NTLM.equals(input);
     }
     
-    static boolean isBearer(String input) {
+    public static boolean isBearer(String input) {
         return OAUTH2_BEARER.equals(input);
     }
 }
