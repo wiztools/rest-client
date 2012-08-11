4c4327edb7c951f2543f3c2c0ded6635a5992aa4
subwiz <subwiz@08545216-953f-0410-99bf-93b34cd2520f>
2012-08-11 14:07:51 +0000

    binary panel implementation completed.
    git-svn-id: https://rest-client.googlecode.com/svn/trunk@731 08545216-953f-0410-99bf-93b34cd2520f

--- restclient-ui/src/main/java/org/wiztools/restclient/ui/resbody/ResBodyBinaryPanel.java
+++ restclient-ui/src/main/java/org/wiztools/restclient/ui/resbody/ResBodyBinaryPanel.java
@@ -1,8 +1,12 @@
 package org.wiztools.restclient.ui.resbody;
 
+import java.awt.GridLayout;
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
+import javax.swing.JTextArea;
+import org.wiztools.restclient.ContentType;
 import org.wiztools.restclient.ui.RESTView;
+import org.wiztools.restclient.util.HexDump;
 
 /**
  *
@@ -11,13 +15,27 @@ import org.wiztools.restclient.ui.RESTView;
 public class ResBodyBinaryPanel extends AbstractResBody {
     @Inject private RESTView view;
     
+    private JTextArea jta = new JTextArea();
+    
     @PostConstruct
     protected void init() {
-        // 
+        jta.setEditable(false);
+        setLayout(new GridLayout());
+        add(jta);
+    }
+
+    @Override
+    public void setBody(byte[] body, ContentType type) {
+        // Call the super method
+        super.setBody(body, type);
+        
+        // Set the hex display value
+        jta.setText(HexDump.getHexDataDumpAsString(body));
+        jta.setCaretPosition(0);
     }
 
     @Override
     public void clearUI() {
-        // do nothing!
+        jta.setText("");
     }
 }
