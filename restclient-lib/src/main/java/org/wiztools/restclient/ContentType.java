90c2b4bd0f61b4c4c59638111b8febdd360cc40f
subwiz <subwiz@08545216-953f-0410-99bf-93b34cd2520f>
2012-08-11 09:52:33 +0000

    contenttype bean added.
    git-svn-id: https://rest-client.googlecode.com/svn/trunk@711 08545216-953f-0410-99bf-93b34cd2520f

---
+++ restclient-lib/src/main/java/org/wiztools/restclient/ContentType.java
@@ -0,0 +1,12 @@
+package org.wiztools.restclient;
+
+import java.nio.charset.Charset;
+
+/**
+ *
+ * @author subwiz
+ */
+public interface ContentType {
+    String getContentType();
+    Charset getCharset();
+}
---
+++ restclient-lib/src/main/java/org/wiztools/restclient/ContentTypeBean.java
@@ -0,0 +1,29 @@
+package org.wiztools.restclient;
+
+import java.nio.charset.Charset;
+
+/**
+ *
+ * @author subwiz
+ */
+public class ContentTypeBean implements ContentType {
+    
+    private String contentType;
+    private Charset charset;
+
+    public ContentTypeBean(String contentType, Charset charset) {
+        this.contentType = contentType;
+        this.charset = charset;
+    }
+
+    @Override
+    public String getContentType() {
+        return contentType;
+    }
+
+    @Override
+    public Charset getCharset() {
+        return charset;
+    }
+    
+}
