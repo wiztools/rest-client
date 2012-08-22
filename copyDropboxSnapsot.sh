e3d031b74559301da515ec5a01b8d9c493a8312f
Subhash Chandran <subwiz@gmail.com>
2012-08-22 12:57:06 +0530

    adding shell script to copy to dropbox

---
+++ copyDropboxSnapsot.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+VERSION=3.0-SNAPSHOT
+
+cp restclient-cli/target/restclient-cli-${VERSION}-jar-with-dependencies.jar \
+  restclient-ui/target/restclient-ui-${VERSION}-jar-with-dependencies.jar \
+  restclient-ui/target/restclient-ui-${VERSION}-app.zip \
+  ~/Dropbox/Public/
+
