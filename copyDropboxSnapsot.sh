1d6539eaeb5b9e25a8bb2a395437a2cd60f07944
Subhash Chandran <subwiz@gmail.com>
2012-09-11 11:50:39 +0530

    version changed in script

--- copyDropboxSnapsot.sh
+++ copyDropboxSnapsot.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-VERSION=3.0-SNAPSHOT
+VERSION=3.1-SNAPSHOT
 
 cp restclient-cli/target/restclient-cli-${VERSION}-jar-with-dependencies.jar \
   restclient-ui/target/restclient-ui-${VERSION}-jar-with-dependencies.jar \
