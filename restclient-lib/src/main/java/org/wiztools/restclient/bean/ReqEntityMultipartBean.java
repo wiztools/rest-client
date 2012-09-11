b32780bd7d0b9c8f511c98edd81f2788f245848e
Subhash Chandran <subwiz@gmail.com>
2012-09-11 10:50:48 +0530

    equals, hashCode and toString implemented.

--- restclient-lib/src/main/java/org/wiztools/restclient/bean/ReqEntityMultipartBean.java
+++ restclient-lib/src/main/java/org/wiztools/restclient/bean/ReqEntityMultipartBean.java
@@ -24,4 +24,33 @@ public class ReqEntityMultipartBean implements ReqEntityMultipart {
     public Object clone() {
         return null;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final ReqEntityMultipartBean other = (ReqEntityMultipartBean) obj;
+        if (this.parts != other.parts && (this.parts == null || !this.parts.equals(other.parts))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 59 * hash + (this.parts != null ? this.parts.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("@ReqEntityMultipart[").append(parts).append("]");
+        return sb.toString();
+    }
 }
