ebc13b50dce5a3cb095142e6caaaa217b38e48b1
Subhash Chandran <subwiz@gmail.com>
2013-08-31 15:53:57 +0530

    version updated

--- chmod.sh
+++ chmod.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-RC_VERSION=3.1.1-SNAPSHOT
+RC_VERSION=3.2.1-SNAPSHOT
 
 chmod a+x restclient-ui/target/restclient-ui-${RC_VERSION}/WizTools.org\ RESTClient.app/Contents/MacOS/JavaApplicationStub
 
