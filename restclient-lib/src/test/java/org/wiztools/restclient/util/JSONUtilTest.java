c4afa58ad01e5fab53f38dc25581ea845bbc27cb
Subhash Chandran <subwiz@gmail.com>
2013-12-04 03:50:20 -0800

    Issue 193 windows test failure fix

--- restclient-lib/src/test/java/org/wiztools/restclient/util/JSONUtilTest.java
+++ restclient-lib/src/test/java/org/wiztools/restclient/util/JSONUtilTest.java
@@ -1,5 +1,7 @@
 package org.wiztools.restclient.util;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import java.io.File;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -34,6 +36,37 @@ public class JSONUtilTest {
     @After
     public void tearDown() {
     }
+    
+    public static class Issue191Bean {
+        private String date;
+        private String performance;
+        private String totalValue;
+
+        public String getDate() {
+            return date;
+        }
+
+        public void setDate(String date) {
+            this.date = date;
+        }
+
+        public String getPerformance() {
+            return performance;
+        }
+
+        public void setPerformance(String performance) {
+            this.performance = performance;
+        }
+
+        public String getTotalValue() {
+            return totalValue;
+        }
+
+        public void setTotalValue(String totalValue) {
+            this.totalValue = totalValue;
+        }
+        
+    }
 
     /**
      * Test of indentJSON method, of class JSONUtil.
@@ -43,10 +76,15 @@ public class JSONUtilTest {
         System.out.println("indentJSON");
         String jsonIn = FileUtil.getContentAsString(
                 new File("src/test/resources/issue_191/one-line.json"), Charsets.UTF_8);
-        String expResult = FileUtil.getContentAsString(
-                new File("src/test/resources/issue_191/indented.json"),
-                Charsets.UTF_8).trim();
-        String result = JSONUtil.indentJSON(jsonIn);
+        
+        String expResult = "1.015786164055542498";
+        
+        String resultJson = JSONUtil.indentJSON(jsonIn);
+        Gson gson = new GsonBuilder().create();
+        Issue191Bean indentedObj = gson.fromJson(resultJson, Issue191Bean.class);
+        String result = indentedObj.performance;
+        
+        // Test loss of double precision in indented json:
         assertEquals(expResult, result);
     }
     
