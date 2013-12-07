331f478c15e9528b1b48d33e8584a6a8d18a61e5
Subhash Chandran <subwiz@gmail.com>
2013-12-07 05:53:14 -0800

    [Issue 193] Fixing the Windows test failure.

--- restclient-lib/src/test/java/org/wiztools/restclient/util/XMLIndentUtilTest.java
+++ restclient-lib/src/test/java/org/wiztools/restclient/util/XMLIndentUtilTest.java
@@ -2,10 +2,10 @@ package org.wiztools.restclient.util;
 
 import static org.junit.Assert.assertEquals;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.File;
 import org.junit.Test;
+import org.wiztools.commons.Charsets;
+import org.wiztools.commons.FileUtil;
 
 /**
  * Test of {@link XMLIndentUtil}.
@@ -18,8 +18,10 @@ public class XMLIndentUtilTest {
      */
     @Test
     public void testGetIndentedUTF() throws Exception {
-        String inXml = this.readTextFile("input_UTF.xml");
-        String expectedResult = this.readTextFile("input_UTF.xml");
+        String inXml = FileUtil.getContentAsString(
+                new File("src/test/resources/input_UTF.xml"), Charsets.UTF_8);
+        String expectedResult = FileUtil.getContentAsString(
+                new File("src/test/resources/input_UTF.xml"), Charsets.UTF_8);
         String result = XMLIndentUtil.getIndented(inXml);
         System.out.println("Expected:\n" + expectedResult);
         System.out.println("Is:\n" + result);
@@ -31,8 +33,10 @@ public class XMLIndentUtilTest {
      */
     @Test
     public void testGetIndentedISO() throws Exception {
-        String inXml = this.readTextFile("input_ISO.xml");
-        String expectedResult = this.readTextFile("input_ISO.xml");
+        String inXml = FileUtil.getContentAsString(
+                new File("src/test/resources/input_ISO.xml"), Charsets.UTF_8);
+        String expectedResult = FileUtil.getContentAsString(
+                new File("src/test/resources/input_ISO.xml"), Charsets.UTF_8);
         String result = XMLIndentUtil.getIndented(inXml);
         System.out.println("Expected:\n" + expectedResult);
         System.out.println("Is:\n" + result);
@@ -44,31 +48,13 @@ public class XMLIndentUtilTest {
      */
     @Test
     public void testGetIndentedNONE() throws Exception {
-        String inXml = this.readTextFile("input_NONE.xml");
-        String expectedResult = this.readTextFile("input_UTF.xml");
+        String inXml = FileUtil.getContentAsString(
+                new File("src/test/resources/input_NONE.xml"), Charsets.US_ASCII);
+        String expectedResult = FileUtil.getContentAsString(
+                new File("src/test/resources/input_UTF.xml"), Charsets.UTF_8);
         String result = XMLIndentUtil.getIndented(inXml);
         System.out.println("Expected:\n" + expectedResult);
         System.out.println("Is:\n" + result);
         assertEquals(expectedResult, result);
     }
-
-    /**
-     * Read a file and return its content as text.
-     *
-     * @param fileName file to read
-     * @return the file's content as text
-     */
-    private String readTextFile(String fileName) throws IOException {
-        InputStream in = this.getClass().getClassLoader().getResourceAsStream(fileName);
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        byte[] data = new byte[BUFFER_SIZE];
-        int count;
-        while ((count = in.read(data, 0, BUFFER_SIZE)) != -1) {
-            out.write(data, 0, count);
-        }
-        System.out.println("Read " + out.size() + " bytes from " + fileName);
-
-        return out.toString();
-    }
-
 }
