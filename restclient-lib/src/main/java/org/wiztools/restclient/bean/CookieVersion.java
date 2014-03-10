c233ccc3487a2239d4e27c80082827d99abfaf10
Subhash Chandran <subwiz@gmail.com>
2014-03-10 20:56:53 +0530

    adding getInt method

--- restclient-lib/src/main/java/org/wiztools/restclient/bean/CookieVersion.java
+++ restclient-lib/src/main/java/org/wiztools/restclient/bean/CookieVersion.java
@@ -6,4 +6,13 @@ package org.wiztools.restclient.bean;
  */
 public enum CookieVersion {
     V_0, V_1;
+    
+    public int getInt() {
+        if(this.equals(V_0)) {
+            return 0;
+        }
+        else {
+            return 1;
+        }
+    }
 }
