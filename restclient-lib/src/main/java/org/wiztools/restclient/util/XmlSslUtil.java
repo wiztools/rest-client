5a7da9002f84553623ca09d51ffcaa2dc25ca7d0
Subhash Chandran <subwiz@gmail.com>
2014-04-26 13:28:40 +0530

    #11 pkcs12 persistence support added.

--- restclient-lib/src/main/java/org/wiztools/restclient/util/XmlSslUtil.java
+++ restclient-lib/src/main/java/org/wiztools/restclient/util/XmlSslUtil.java
@@ -5,6 +5,7 @@ import nu.xom.Attribute;
 import nu.xom.Element;
 import nu.xom.Elements;
 import org.wiztools.commons.StringUtil;
+import org.wiztools.restclient.bean.KeyStoreType;
 import org.wiztools.restclient.bean.SSLHostnameVerifier;
 import org.wiztools.restclient.bean.SSLReq;
 import org.wiztools.restclient.bean.SSLReqBean;
@@ -33,6 +34,7 @@ class XmlSslUtil {
         // Key store
         if(req.getKeyStore() != null) {
             Element e = new Element("keystore");
+            e.addAttribute(new Attribute("type", req.getKeyStoreType().name()));
             e.addAttribute(new Attribute("file", req.getKeyStore().getAbsolutePath()));
             e.addAttribute(new Attribute("password", Util.base64encode(new String(req.getKeyStorePassword()))));
             eSsl.appendChild(e);
@@ -41,6 +43,7 @@ class XmlSslUtil {
         // Trust store
         if(req.getTrustStore() != null) {
             Element e = new Element("truststore");
+            e.addAttribute(new Attribute("type", req.getTrustStoreType().name()));
             e.addAttribute(new Attribute("file", req.getTrustStore().getAbsolutePath()));
             e.addAttribute(new Attribute("password", Util.base64encode(new String(req.getTrustStorePassword()))));
             eSsl.appendChild(e);
@@ -56,19 +59,31 @@ class XmlSslUtil {
         for(int i=0; i<eChildren.size(); i++) {
             Element e = eChildren.get(i);
             final String name = e.getLocalName();
-            if("trust-self-signed-cert".equals(name)) {
-                out.setTrustSelfSignedCert(true);
-            }
-            else if("hostname-verifier".equals(name)) {
-                out.setHostNameVerifier(SSLHostnameVerifier.valueOf(e.getValue()));
-            }
-            else if("keystore".equals(name)) {
-                out.setKeyStore(new File(e.getAttributeValue("file")));
-                out.setKeyStorePassword(Util.base64decode(e.getAttributeValue("password")).toCharArray());
-            }
-            else if("truststore".equals(name)) {
-                out.setTrustStore(new File(e.getAttributeValue("file")));
-                out.setTrustStorePassword(Util.base64decode(e.getAttributeValue("password")).toCharArray());
+            if(null != name) switch (name) {
+                case "trust-self-signed-cert":
+                    out.setTrustSelfSignedCert(true);
+                    break;
+                case "hostname-verifier":
+                    out.setHostNameVerifier(SSLHostnameVerifier.valueOf(e.getValue()));
+                    break;
+                case "keystore":
+                    { // type:
+                        final String typeStr = e.getAttributeValue("type");
+                        if(StringUtil.isNotEmpty(typeStr))
+                            out.setKeyStoreType(KeyStoreType.valueOf(typeStr));
+                    }
+                    out.setKeyStore(new File(e.getAttributeValue("file")));
+                    out.setKeyStorePassword(Util.base64decode(e.getAttributeValue("password")).toCharArray());
+                    break;
+                case "truststore":
+                    { // type:
+                        final String typeStr = e.getAttributeValue("type");
+                        if(StringUtil.isNotEmpty(typeStr))
+                            out.setTrustStoreType(KeyStoreType.valueOf(typeStr));
+                    }
+                    out.setTrustStore(new File(e.getAttributeValue("file")));
+                    out.setTrustStorePassword(Util.base64decode(e.getAttributeValue("password")).toCharArray());
+                    break;
             }
         }
         
