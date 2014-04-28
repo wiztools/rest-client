1635c639ee6397580d7e00010b8b06c1a5ab1c5a
Subhash Chandran <subwiz@gmail.com>
2014-04-28 18:21:52 +0530

    version taken from pom

--- copyDropboxSnapsot.sh
+++ copyDropboxSnapsot.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-VERSION=3.1-SNAPSHOT
+VERSION=`head pom.xml | grep 'version.*version' | sed -E 's/.*version.(.*)..version.*/\1/g'`
 
 cp restclient-cli/target/restclient-cli-${VERSION}-jar-with-dependencies.jar \
   restclient-ui/target/restclient-ui-${VERSION}-jar-with-dependencies.jar \
-  restclient-ui/target/restclient-ui-${VERSION}-app.zip \
   ~/Dropbox/Public/
 
