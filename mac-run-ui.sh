69ef82d253df28f27cf4ea247328ff86194e5d17
Subhash Chandran <subwiz@gmail.com>
2014-05-01 15:12:15 +0530

    enabled wire logging for debug purposes

--- mac-run-ui.sh
+++ mac-run-ui.sh
@@ -2,4 +2,4 @@
 
 VERSION=`head pom.xml | grep 'version.*version' | sed -E 's/.*version.(.*)..version.*/\1/g'`
 
-java -Dapple.laf.useScreenMenuBar=true  -Dapple.awt.application.name=RESTClient -jar restclient-ui/target/restclient-ui-$VERSION-jar-with-dependencies.jar
+java -Dorg.apache.commons.logging.Log=org.apache.commons.logging.impl.SimpleLog -Dorg.apache.commons.logging.simplelog.log.org.apache.http.wire=DEBUG -Dapple.laf.useScreenMenuBar=true  -Dapple.awt.application.name=RESTClient -jar restclient-ui/target/restclient-ui-$VERSION-jar-with-dependencies.jar
