445e3d53d3566d347639e9442d9e5e72765b5041
Subhash Chandran <subwiz@gmail.com>
2014-08-05 06:46:49 +0530

    updated to copy .dmg file

--- copyDropboxSnapsot.sh
+++ copyDropboxSnapsot.sh
@@ -6,3 +6,7 @@ cp restclient-cli/target/restclient-cli-${VERSION}-jar-with-dependencies.jar \
   restclient-ui/target/restclient-ui-${VERSION}-jar-with-dependencies.jar \
   ~/Dropbox/Public/
 
+if [ -e restclient-ui/target/RESTClient-${VERSION}.dmg ]; then
+  cp restclient-ui/target/RESTClient-${VERSION}.dmg ~/Dropbox/Public/
+fi
+
