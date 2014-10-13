b1bdedb02d6a8a0cf9ac2d3ba4494a7cbd28482c
Subhash Chandran <subwiz@gmail.com>
2014-10-13 21:16:50 +0530

    added menu control

---
+++ restclient-jfx/src/main/java/org/wiztools/restclient/jfx/MenuController.java
@@ -0,0 +1,22 @@
+package org.wiztools.restclient.jfx;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author subwiz
+ */
+public class MenuController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
