3c5ce012d391fff88cb6ce6e734ef312c50422ee
Subhash Chandran <subwiz@gmail.com>
2015-03-23 16:03:06 +0530

    simplified coding

--- restclient-lib/src/main/java/org/wiztools/restclient/HTTPClientRequestExecuter.java
+++ restclient-lib/src/main/java/org/wiztools/restclient/HTTPClientRequestExecuter.java
@@ -361,18 +361,14 @@ public class HTTPClientRequestExecuter implements RequestExecuter {
             if(sslReq != null) {
                 SSLHostnameVerifier verifier = sslReq.getHostNameVerifier();
                 final HostnameVerifier hcVerifier;
-                switch(verifier){
-                    case STRICT:
-                        // hcVerifier = SSLConnectionSocketFactory.STRICT_HOSTNAME_VERIFIER;
-                        hcVerifier = new DefaultHostnameVerifier();
-                        break;
-                    case BROWSER_COMPATIBLE:
-                        // hcVerifier = SSLConnectionSocketFactory.BROWSER_COMPATIBLE_HOSTNAME_VERIFIER;
-                        hcVerifier = new DefaultHostnameVerifier();
-                        break;
+                // hcVerifier = SSLConnectionSocketFactory.STRICT_HOSTNAME_VERIFIER;
+                // hcVerifier = SSLConnectionSocketFactory.BROWSER_COMPATIBLE_HOSTNAME_VERIFIER;
+                switch(verifier) {
                     case ALLOW_ALL:
                         hcVerifier = new AllowAllHostnameVerifier();
                         break;
+                    case STRICT:
+                    case BROWSER_COMPATIBLE:
                     default:
                         hcVerifier = new DefaultHostnameVerifier();
                         break;
