220a780d2672cd9fe75c9727ac7ef9b25f820480
Subhash Chandran <subwiz@gmail.com>
2016-05-05 10:30:14 +0530

    now running from gradle build.

--- mac-run-ui.sh
+++ mac-run-ui.sh
@@ -7,4 +7,8 @@ if [ -n "$JAVA_HOME" ]; then
     jcmd=$JAVA_HOME/bin/java
 fi
 
-$jcmd -Dorg.apache.commons.logging.Log=org.apache.commons.logging.impl.SimpleLog -Dorg.apache.commons.logging.simplelog.log.org.apache.http.wire=DEBUG -Dapple.laf.useScreenMenuBar=true  -Dapple.awt.application.name=RESTClient -jar restclient-ui/target/restclient-ui-$VERSION-jar-with-dependencies.jar
+$jcmd -Dorg.apache.commons.logging.Log=org.apache.commons.logging.impl.SimpleLog \
+  -Dorg.apache.commons.logging.simplelog.log.org.apache.http.wire=DEBUG \
+  -Dapple.laf.useScreenMenuBar=true  \
+  -Dapple.awt.application.name=RESTClient \
+  -jar restclient-ui/build/libs/restclient-ui-fat-$VERSION.jar
