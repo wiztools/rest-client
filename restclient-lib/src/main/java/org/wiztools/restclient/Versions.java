cece09fabd3432a73e52ff147af710aba57eda6f
Subhash Chandran <subwiz@gmail.com>
2016-05-29 14:49:41 +0530

    simplified version check.

--- restclient-lib/src/main/java/org/wiztools/restclient/Versions.java
+++ restclient-lib/src/main/java/org/wiztools/restclient/Versions.java
@@ -1,6 +1,7 @@
 package org.wiztools.restclient;
 
-import java.util.Arrays;
+import org.wiztools.appupdate.Version;
+import org.wiztools.appupdate.VersionImpl;
 
 /**
  *
@@ -9,32 +10,23 @@ import java.util.Arrays;
 public final class Versions {
     public static final String CURRENT = "3.6";
     
-    private static final String[] VERSIONS = new String[]{
-        "3.0", "3.1", "3.2", "3.2.1", "3.2.2", "3.2.3", "3.3", "3.3.1", "3.4",
-        "3.4.1", "3.4.2", "3.5", CURRENT
-    };
-    
-    static {
-        // Sort the version array for binary search
-        Arrays.sort(VERSIONS);
-    }
+    private static final Version MIN = new VersionImpl("3.0");
+    private static final Version MAX = new VersionImpl(CURRENT);
     
     public static void versionValidCheck(final String restVersion)
             throws VersionValidationException {
         if (restVersion == null) {
             throw new VersionValidationException("Attribute `version' not available for root element");
         }
-        int res = Arrays.binarySearch(VERSIONS, restVersion);
-        if (res == -1) {
+        Version stored = new VersionImpl(restVersion);
+        if(!stored.isWithin(MIN, MAX)) {
             throw new VersionValidationException("Version not supported");
         }
     }
     
     public static class VersionValidationException extends Exception {
-
         public VersionValidationException(String message) {
             super(message);
         }
-        
     }
 }
