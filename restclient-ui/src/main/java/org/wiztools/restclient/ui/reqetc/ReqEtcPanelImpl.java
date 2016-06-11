9c687e65119f0c8ed615a4443ac65adf5d7492ec
Subhash Chandran <subwiz@gmail.com>
2016-06-11 22:48:30 +0530

    made the checkbox layout consistent.

--- restclient-ui/src/main/java/org/wiztools/restclient/ui/reqetc/ReqEtcPanelImpl.java
+++ restclient-ui/src/main/java/org/wiztools/restclient/ui/reqetc/ReqEtcPanelImpl.java
@@ -13,53 +13,51 @@ import org.wiztools.restclient.bean.HTTPVersion;
  * @author subwiz
  */
 public class ReqEtcPanelImpl extends JPanel implements ReqEtcPanel {
-    
+
     // HTTP Version Combo box
     private final JComboBox<HTTPVersion> jcb_http_version =
             new JComboBox<>(HTTPVersion.values());
-    
+
     // Cookie versions
     private final JComboBox<CookieVersion> jcb_cookie_version =
             new JComboBox<>(CookieVersion.values());
 
     // Follow redirect
     private final JCheckBox jcb_followRedirects = new JCheckBox("Follow HTTP Redirects? ");
-    
+
     // Ignore body
     private final JCheckBox jcb_ignoreResponseBody = new JCheckBox("Ignore Response Body? ");
-    
+
     @PostConstruct
     protected void init() {
         JPanel jp_etc = new JPanel(new GridLayout(3, 1));
         { // Http Version
             JPanel jp = new JPanel(new FlowLayout(FlowLayout.LEFT));
             jp.add(new JLabel("Version: "));
-            
+
             jp.add(jcb_http_version);
             jp.add(jcb_cookie_version);
-            
+
             jp_etc.add(jp);
         }
-        
+
         { // Follow Redirect
             JPanel jp = new JPanel(new FlowLayout(FlowLayout.LEFT));
             // Previous version of RESTClient had follow redirects as true.
             // To maintain backward compatibility in default behavior:
             jcb_followRedirects.setSelected(true);
-            jcb_followRedirects.setHorizontalTextPosition(SwingConstants.LEFT);
             jcb_followRedirects.setBorder(null);
             jp.add(jcb_followRedirects);
             jp_etc.add(jp);
         }
-        
+
         { // Ignore response body
             JPanel jp = new JPanel(new FlowLayout(FlowLayout.LEFT));
-            jcb_ignoreResponseBody.setHorizontalTextPosition(SwingConstants.LEFT);
             jcb_ignoreResponseBody.setBorder(null);
             jp.add(jcb_ignoreResponseBody);
             jp_etc.add(jp);
         }
-        
+
         setLayout(new FlowLayout(FlowLayout.LEFT));
         add(jp_etc);
     }
@@ -118,5 +116,5 @@ public class ReqEtcPanelImpl extends JPanel implements ReqEtcPanel {
     public void setIgnoreResponseBody(boolean b) {
         jcb_ignoreResponseBody.setSelected(b);
     }
-    
+
 }
